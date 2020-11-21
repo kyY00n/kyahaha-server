518aea8afd039fbaffce87e39e590ed5d02080bb
Woo-yeong Kim <kimwy1997@gmail.com>
2020-11-22 02:55:39 +0900

    Fix CORS error

--- app.js
+++ app.js
@@ -23,11 +23,23 @@ var app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
-app.use(cors());
+// app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/api", indexRouter);
